a4413ce1e330d4aeb5f87ee52f2e57be6a9ac14b
DavidFede <david.sandarivskyj@student.spsmb.cz>
2023-10-31 15:08:19 +0100

    commit

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,6 @@
 const point = document.getElementById("point");
 const startGame = document.getElementById("startGame");
+const timeInfo = document.getElementById("timeInfo");
 
 let pointInterval;
 
@@ -11,21 +12,33 @@ startGame.onclick = () => {
 const movePoint = (element, x, y) => {
     element.style.top = `${y}px`;
     element.style.left = `${x}px`;
+    console.log(parseFloat(point.style.width));
 }
 
 const startPointInterval = () =>{
     clearInterval(pointInterval);
     setPointClick(point);
     pointInterval = setInterval(() => {
-        movePoint(point, getRandomNumber(100, 600), getRandomNumber(100,600));
-    }, 600);
+        setSize(point, getRandomNumber(40,70))
+        movePoint(point, getRandomNumber(parseFloat(point.style.width), window.innerWidth - parseFloat(point.style.width)), getRandomNumber(parseFloat(point.style.height), window.innerHeight - parseFloat(point.style.width)));
+    }, 1000);
 }
 const getRandomNumber = (minimum, maximum) =>
 Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+let timeStart = 0;
 const setPointClick = (element) => {
     element.onclick = () => {
+        if(timeStart == 0) {
+            timeStart = performance.now();
+        } else{
+            let timeEnd = performance.now();
+            let result = timeEnd - timeStart;
+            timeInfo.innerText = `Time: ${result}ms`
+            timeStart = performance.now();
+        }
         element.innerText++;
+        movePoint(point, getRandomNumber(0, window.innerWidth), getRandomNumber(0, window.innerHeight));
     }
 }
 
